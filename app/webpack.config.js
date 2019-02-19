e46058b74e4e166c1283de17f4b838abb9aa1062
Gry0u <gauthriou@protonmail.com>
2019-02-19 14:37:56 +0100

    Add HTML and JS files

---
+++ app/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: ['url-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true },
+};
