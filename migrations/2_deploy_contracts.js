afd8ca0eaaf180353fea2e26d913e96d58297a00
Gry0u <gauthriou@protonmail.com>
2019-02-19 14:43:51 +0100

    Set specific deployer address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,5 @@
 const SupplyChain = artifacts.require('./SupplyChain.sol')
 
 module.exports = function (deployer) {
-  deployer.deploy(SupplyChain)
+  deployer.deploy(SupplyChain, { from: '0x79078A5671D4B53B37BA8e03f1E53950DbF3818F' })
 }
