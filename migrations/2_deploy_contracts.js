8c1428384c39d444a88c07e4617c0178da05b9c5
Gry0u <gauthriou@protonmail.com>
2019-02-20 11:18:42 +0100

    Update truffle config

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
 const SupplyChain = artifacts.require('./SupplyChain.sol')
-const fs = require('fs')
-
-const account = fs.readFileSync("../app/owner_account").toString().trim()
 
 module.exports = function (deployer) {
-  deployer.deploy(SupplyChain, { from: account })
+  deployer.deploy(SupplyChain)
 }
