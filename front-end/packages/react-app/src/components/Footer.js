bac4f46b7268ca53b1352ea3ee352435734b400b
sripwoud <me@sripwoud.xyz>
2020-05-14 23:05:32 +0200

    Add Layout (Header & Footer)

---
+++ front-end/packages/react-app/src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Flex, Link as LinkRimble } from 'rimble-ui'
+import GitHubIcon from '@material-ui/icons/GitHub'
+
+export default () => (
+  <Flex
+    alignItems='center'
+    justifyContent='center'
+    flexDirection='column'
+  >
+    <p>Made by
+      <LinkRimble
+        href='https://twitter.com/r1oga'
+        target='__blank'
+        px={2}
+      >@r1oga
+      </LinkRimble>
+    </p>
+    <LinkRimble
+      href='https://github.com/r1oga/AirChain'
+      target='__blank'
+      px={2}
+    >
+      <GitHubIcon fontSize='large' />
+    </LinkRimble>
+  </Flex>
+)
