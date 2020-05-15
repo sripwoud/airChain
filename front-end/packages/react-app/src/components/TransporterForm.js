037c39fd1438479931f4dfc0bd0f45bc5d4f300c
sripwoud <me@sripwoud.xyz>
2020-05-15 22:27:07 +0200

    Draft Transporter Form

--- front-end/packages/react-app/src/components/TransporterForm.js
+++ front-end/packages/react-app/src/components/TransporterForm.js
@@ -11,25 +11,15 @@ import {
 
 export default props => {
   const [equipmentId, setEquipmentId] = useState(0)
-  const [manufacturer, setManufacturer] = useState('')
-  const [msn, setMsn] = useState(0)
 
-  const data = { equipmentId, manufacturer }
-
-  const onOrder = async event => {
+  const onSubmit = async event => {
     event.preventDefault()
-    console.log(data)
-  }
-
-  const onReceive = async event => {
-    event.preventDefault()
-    console.log(msn)
   }
 
   return (
     <>
       <Heading.h2>Transporter actions</Heading.h2>
-      <Form onSubmit={onOrder}>
+      <Form onSubmit={onSubmit}>
         <Flex alignItems='center' justifyContent='space-between'>
           <Field
             label='Equipment ID'
@@ -45,53 +35,13 @@ export default props => {
               onChange={event => setEquipmentId(event.target.value)}
             />
           </Field>
-          <Field
-            label='Manufacturer Address'
-            width={1 / 2}
-          >
-            <Input
-              value={manufacturer}
-              type='text'
-              required
-              placeholder='0x...'
-              width={1}
-              onChange={event => setManufacturer(event.target.value)}
-            />
-          </Field>
           <Button
             type='submit'
             width={1 / 2}
             mt={3}
             mx={2}
           >
-            Order
-          </Button>
-        </Flex>
-      </Form>
-      <Form onSubmit={onReceive}>
-        <Flex justifyContent='space-between'>
-          <Field
-            label='MSN'
-            width={1 / 3}
-            ml={2}
-            pr={3}
-          >
-            <Input
-              value={msn}
-              type='number'
-              required
-              placeholder='MSN'
-              width={1}
-              onChange={event => setMsn(event.target.value)}
-            />
-          </Field>
-          <Button
-            type='submit'
-            width={1 / 3}
-            mt='27px'
-            mr={2}
-          >
-            Receive
+            Transport Equipment
           </Button>
         </Flex>
       </Form>
